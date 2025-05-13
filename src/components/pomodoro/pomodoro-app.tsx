f1c9e9b4876a3fe3e598b45205e81fcdde1cd50e
SND <99936282+snd-primary@users.noreply.github.com>
2025-05-14 02:46:02 +0900

    Add InstallPWA component to PomodoroApp

--- src/components/pomodoro/pomodoro-app.tsx
+++ src/components/pomodoro/pomodoro-app.tsx
@@ -4,16 +4,20 @@ import { Settings } from "./setting";
 import { Controls } from "./controls";
 import { CompletedCounter } from "./completed-counter";
 import { Timer } from "./timer";
+import { InstallPWA } from "@/components/install-pwa";
 
 export const PomodoroApp = () => {
-	return (
-		<main className="mx-auto max-w-full w-full h-fit grid grid-cols-1 grid-rows-[1fr_70px] gap-6 items-center justify-center bg-background text-foreground px-4 relative">
-			<Settings />
-			<div className="w-full grid grid-cols-1 h-96 gap-16 md:gap-8 items-end justify-center justify-self-center sm:border-1  sm:p-6 rounded-sm relative">
-				<Timer />
-				<Controls />
-			</div>
-			<CompletedCounter />
-		</main>
-	);
+  return (
+    <>
+      <main className="mx-auto max-w-full w-full h-fit grid grid-cols-1 grid-rows-[1fr_70px] gap-6 items-center justify-center bg-background text-foreground px-4 relative">
+        <Settings />
+        <div className="w-full grid grid-cols-1 h-96 gap-16 md:gap-8 items-end justify-center justify-self-center sm:border-1  sm:p-6 rounded-sm relative">
+          <Timer />
+          <Controls />
+        </div>
+        <CompletedCounter />
+      </main>
+      <InstallPWA />
+    </>
+  );
 };
